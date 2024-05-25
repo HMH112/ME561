cd29d8f5433cae701310dcf56d3b74226e118cdd
HMH112 <percynal1233@gmail.com>
2024-05-25 18:16:35 +0300

    Changed values of Hough Lines

--- lane.py
+++ lane.py
@@ -76,7 +76,7 @@ while(cap.isOpened()):
     _, frame = cap.read()
     canny_image = canny(frame)
     cropped_image = region_of_interest(canny_image)
-    lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100, np.array([]), minLineLength=40, maxLineGap=5)
+    lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100, np.array([]), minLineLength=80, maxLineGap=10)
     averaged_lines = average_slope_intercept(frame, lines)
     line_image = display_lines(frame, averaged_lines)
     combo_image = cv2.addWeighted(frame, 0.8, line_image, 1, 1)
